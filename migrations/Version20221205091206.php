3a4d25074501990b1f8919a35fbb7306e625dfa1
Nefferou <fertijulien@gmail.com>
2022-12-05 10:12:20 +0100

    Relation

---
+++ migrations/Version20221205091206.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221205091206 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__album AS SELECT id, title, date FROM album');
+        $this->addSql('DROP TABLE album');
+        $this->addSql('CREATE TABLE album (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, artiste_id INTEGER DEFAULT NULL, title VARCHAR(255) NOT NULL, date DATE NOT NULL, CONSTRAINT FK_39986E4321D25844 FOREIGN KEY (artiste_id) REFERENCES artiste (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO album (id, title, date) SELECT id, title, date FROM __temp__album');
+        $this->addSql('DROP TABLE __temp__album');
+        $this->addSql('CREATE INDEX IDX_39986E4321D25844 ON album (artiste_id)');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__song AS SELECT id, title, length FROM song');
+        $this->addSql('DROP TABLE song');
+        $this->addSql('CREATE TABLE song (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, album_id INTEGER DEFAULT NULL, title VARCHAR(255) NOT NULL, length INTEGER NOT NULL, CONSTRAINT FK_33EDEEA11137ABCF FOREIGN KEY (album_id) REFERENCES album (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO song (id, title, length) SELECT id, title, length FROM __temp__song');
+        $this->addSql('DROP TABLE __temp__song');
+        $this->addSql('CREATE INDEX IDX_33EDEEA11137ABCF ON song (album_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__album AS SELECT id, title, date FROM album');
+        $this->addSql('DROP TABLE album');
+        $this->addSql('CREATE TABLE album (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, date DATE NOT NULL)');
+        $this->addSql('INSERT INTO album (id, title, date) SELECT id, title, date FROM __temp__album');
+        $this->addSql('DROP TABLE __temp__album');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__song AS SELECT id, title, length FROM song');
+        $this->addSql('DROP TABLE song');
+        $this->addSql('CREATE TABLE song (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, length INTEGER NOT NULL)');
+        $this->addSql('INSERT INTO song (id, title, length) SELECT id, title, length FROM __temp__song');
+        $this->addSql('DROP TABLE __temp__song');
+    }
+}
